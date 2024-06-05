22a7bc7c5e2fe4cc45c0e8e0944b745636004fbe
Nithin <nithin.govindarajan@gmail.com>
2024-06-05 22:24:22 +0200

    reached to TQSMatrix

--- TQSMatrices.jl
+++ TQSMatrices.jl
@@ -4,8 +4,9 @@ module TQSMatrices
 ###########
 # exports #
 ###########
-export ZeroMatrix, Spinner, GIRS_is_consistent, TQS, IndexedVector, GIRS_is_tree, GIRS_has_no_bounce_back_operators
-
+export ZeroMatrix, Spinner, GIRS_is_consistent, TQSMatrix, IndexedVector, GIRS_is_tree
+export GIRS_has_no_bounce_back_operators, determine_tree_hierarchy, GraphPartitionedMatrix
+export get_block, get_hankelblock
 
 ############
 # packages #
@@ -13,91 +14,164 @@ export ZeroMatrix, Spinner, GIRS_is_consistent, TQS, IndexedVector, GIRS_is_tree
 using LinearAlgebra
 using IterTools
 
-
 ##############
 # ZeroMatrix #
 ##############
-struct ZeroMatrix{Scalar<:Number} <: AbstractMatrix{Scalar}
-    m::UInt
-    n::UInt
+struct ZeroMatrix{Scalar <: Number} <: AbstractMatrix{Scalar}
+	m::UInt
+	n::UInt
 end
-ZeroMatrix(m,n) = ZeroMatrix{Float64}(m,n)
+ZeroMatrix(m, n) = ZeroMatrix{Float64}(m, n)
 Base.:size(A::ZeroMatrix) = (A.m, A.n)
 function Base.:getindex(A::ZeroMatrix, i::Int, j::Int)
-    if 0 < i <= A.m && 0 < j <= A.n
-        return convert(eltype(A), 0)
-    else
-        throw(BoundsError(A, (i, j)))
-    end
+	if 0 < i <= A.m && 0 < j <= A.n
+		return convert(eltype(A), 0)
+	else
+		throw(BoundsError(A, (i, j)))
+	end
 end
 Base.:Matrix(A::ZeroMatrix) = zeros(eltype(A), A.m, A.n)
 
 #################################
 # IndexedVector & IndexedMatrix 
 #################################
-IndexedVector{T<:Any} = Dict{UInt,T}
-IndexedVector{T}(array , labels) where {T<:Any} = Dict{UInt,T}(zip(labels, array)) 
-struct IndexedMatrix{T<:Any}
-    array::Matrix{T}
-    index_map::Dict{UInt,UInt}
-    function IndexedMatrix{T}(array , labels) where {T<:Any}
-        @assert size(array,1) == size(array,2)
-        @assert size(array,1) == length(labels)
-        new{T}(convert(Matrix{T},array),  Dict(label => i for (i, label) in enumerate(labels)) )
-    end
+IndexedVector{T <: Any} = Dict{UInt, T}
+IndexedVector{T}(array, labels) where {T <: Any} = Dict{UInt, T}(zip(labels, array))
+struct IndexedMatrix{T <: Any}
+	array::Matrix{T}
+	index_map::Dict{UInt, UInt}
+	function IndexedMatrix{T}(array, labels) where {T <: Any}
+		@assert size(array, 1) == size(array, 2)
+		@assert size(array, 1) == length(labels)
+		new{T}(convert(Matrix{T}, array), Dict(label => i for (i, label) in enumerate(labels)))
+	end
 end
-Base.:getindex(A::IndexedMatrix,i,j) = A.array[A.index_map[i],A.index_map[j]]
-IndexedMatrix(array,labels) = IndexedMatrix{eltype(array)}(array,labels)
-    
+Base.:getindex(A::IndexedMatrix, i, j) = A.array[A.index_map[i], A.index_map[j]]
+IndexedMatrix(array, labels) = IndexedMatrix{eltype(array)}(array, labels)
+
+
+############################
+# Graph Partitioned matrix #
+############################
+struct GraphPartitionedMatrix{Scalar <: Number}
+	mat::AbstractMatrix
+	# graph attributes
+	nodes::Vector{UInt}
+	K::UInt
+	adjacency_list::IndexedVector{Vector{UInt}}
+	# mapping between nodes and matrix entries
+	mrange::IndexedVector{UnitRange}
+	nrange::IndexedVector{UnitRange}
+	# dimensions
+	m::IndexedVector{UInt}
+	n::IndexedVector{UInt}
+	M::UInt
+	N::UInt
+	function GraphPartitionedMatrix{Scalar}(mat, nodes, m, n, adjacency_list) where {Scalar <: Number}
+		@assert length(m) == length(n) == length(nodes)
+		@assert sum(values(m)) == size(mat, 1)
+		@assert sum(values(n)) == size(mat, 2)
+		@assert length(Set(nodes)) == length(nodes)
+		@assert all([haskey(adjacency_list, node) for node in nodes])
+		@assert length(keys(adjacency_list)) == length(nodes)
+		for neighbors in values(adjacency_list)
+			for n in neighbors
+				@assert haskey(adjacency_list, n)
+			end
+		end
+
+		K = length(nodes)
+		M = size(mat, 1)
+		N = size(mat, 2)
+
+		# m and n
+		m = IndexedVector{UInt}(m, nodes)
+		n = IndexedVector{UInt}(n, nodes)
+
+		# construct mrange
+		mrange = Dict{UInt, UnitRange}()
+		off = 0
+		for node in nodes
+			mrange[node] = (off+1):(off+m[node])
+			off = off + m[node]
+		end
+
+		# construct nrange
+		nrange = Dict{UInt, UnitRange}()
+		off = 0
+		for node in nodes
+			nrange[node] = (off+1):(off+n[node])
+			off = off + n[node]
+		end
+
+		# construct 
+		new{Scalar}(mat, nodes, K, adjacency_list, mrange, nrange, m, n, M, N)
+	end
+end
+GraphPartitionedMatrix(mat, nodes, m, n, adjacency_list) = GraphPartitionedMatrix{eltype(mat)}(mat, nodes, m, n, adjacency_list)
+Base.:size(A::GraphPartitionedMatrix) = (A.M, A.N)
+Base.:getindex(A::GraphPartitionedMatrix, i::Int, j::Int) = A.mat[i, j]
+# extract a block
+function get_block(A::GraphPartitionedMatrix, i, j)
+	rows = vcat([A.mrange[k] for k in i]...)
+	columns = vcat([A.nrange[k] for k in j]...)
+	return A.mat[rows, columns]
+end
+get_block(A::GraphPartitionedMatrix, i::Union{UInt, Vector{UInt}}) = get_block(A, i, i)
+function get_hankelblock(A::GraphPartitionedMatrix, j)
+	i = filter(x -> !(x in j), A.nodes)
+	return get_block(A, i, j)
+end
+
 
 ###########
 # Spinner #
 ###########
-struct Spinner{Scalar<:Number}
-    id::UInt
-    neighbors::Vector{UInt}
-    trans::IndexedMatrix{AbstractMatrix{Scalar}}
-    inp::IndexedVector{AbstractMatrix{Scalar}}
-    out::IndexedVector{AbstractMatrix{Scalar}}
-    D::AbstractMatrix{Scalar}
-    m::Int
-    n::Int
-    p_in::IndexedVector{UInt}
-    p_out::IndexedVector{UInt} 
+struct Spinner{Scalar <: Number}
+	id::UInt
+	neighbors::Vector{UInt}
+	trans::IndexedMatrix{AbstractMatrix{Scalar}}
+	inp::IndexedVector{AbstractMatrix{Scalar}}
+	out::IndexedVector{AbstractMatrix{Scalar}}
+	D::AbstractMatrix{Scalar}
+	m::Int
+	n::Int
+	p_in::IndexedVector{UInt}
+	p_out::IndexedVector{UInt}
 
-    function Spinner{Scalar}(id, neighbors, trans, inp, out, D) where {Scalar<:Number}
-        # spinners cannot have its own node id as neighbor
-        @assert all(x -> x != id, neighbors)
-        # neighbor list is unique
-        @assert length(Set(neighbors)) == length(neighbors)
-        # dimensionality checks
-        @assert length(neighbors) == size(trans, 1)
-        @assert size(trans, 1) == size(trans, 2)
-        @assert length(inp) == size(trans, 1)
-        @assert length(out) == size(trans, 1)
-        # operator dimension checks
-        @assert all([size(el, 1) == size(D, 1) for el in out])
-        @assert all([size(el, 2) == size(D, 2) for el in inp])
-        for iter in eachindex(inp)
-            @assert all([size(el, 1) == size(inp[iter], 1) for el in trans[iter, :]])
-        end
-        for iter in eachindex(out)
-            @assert all([size(el, 2) == size(out[iter], 2) for el in trans[:, iter]])
-        end
-        # construct
-        new{Scalar}(
-            id,
-            neighbors,
-            IndexedMatrix{AbstractMatrix{Scalar}}(trans,neighbors),
-            IndexedVector{AbstractMatrix{Scalar}}(inp, neighbors),
-            IndexedVector{AbstractMatrix{Scalar}}(out, neighbors),
-            convert(AbstractMatrix{Scalar}, D),
-            size(D, 1),
-            size(D, 2),
-            Dict(j => size(c,2) for (j, c) in zip(neighbors,out)),
-            Dict(j => size(b,1) for (j, b) in zip(neighbors,inp)),
-        )
-    end
+	function Spinner{Scalar}(id, neighbors, trans, inp, out, D) where {Scalar <: Number}
+		# spinners cannot have its own node id as neighbor
+		@assert all(x -> x != id, neighbors)
+		# neighbor list is unique
+		@assert length(Set(neighbors)) == length(neighbors)
+		# dimensionality checks
+		@assert length(neighbors) == size(trans, 1)
+		@assert size(trans, 1) == size(trans, 2)
+		@assert length(inp) == size(trans, 1)
+		@assert length(out) == size(trans, 1)
+		# operator dimension checks
+		@assert all([size(el, 1) == size(D, 1) for el in out])
+		@assert all([size(el, 2) == size(D, 2) for el in inp])
+		for iter in eachindex(inp)
+			@assert all([size(el, 1) == size(inp[iter], 1) for el in trans[iter, :]])
+		end
+		for iter in eachindex(out)
+			@assert all([size(el, 2) == size(out[iter], 2) for el in trans[:, iter]])
+		end
+		# construct
+		new{Scalar}(
+			id,
+			neighbors,
+			IndexedMatrix{AbstractMatrix{Scalar}}(trans, neighbors),
+			IndexedVector{AbstractMatrix{Scalar}}(inp, neighbors),
+			IndexedVector{AbstractMatrix{Scalar}}(out, neighbors),
+			convert(AbstractMatrix{Scalar}, D),
+			size(D, 1),
+			size(D, 2),
+			Dict(j => size(c, 2) for (j, c) in zip(neighbors, out)),
+			Dict(j => size(b, 1) for (j, b) in zip(neighbors, inp)),
+		)
+	end
 end
 Base.:size(S::Spinner) = (S.m, S.n)
 IndexedVector{Spinner}(S) = Dict(s.id => s for s in S)
@@ -109,15 +183,15 @@ IndexedVector{Spinner}(S) = Dict(s.id => s for s in S)
 
 
 function GIRS_is_consistent(nodeset)
-    for node in values(nodeset)
-        for neighbor in node.neighbors
-            # node i is a neighbor of node j iff node j is a neighbor of node i
-            @assert node.id in nodeset[neighbor].neighbors
-            # dimension consistency
-            @assert node.p_out[neighbor] == nodeset[neighbor].p_in[node.id]
-        end
-    end
-    return true 
+	for node in values(nodeset)
+		for neighbor in node.neighbors
+			# node i is a neighbor of node j iff node j is a neighbor of node i
+			@assert node.id in nodeset[neighbor].neighbors
+			# dimension consistency
+			@assert node.p_out[neighbor] == nodeset[neighbor].p_in[node.id]
+		end
+	end
+	return true
 end
 
 #########################################
@@ -125,31 +199,31 @@ end
 #########################################
 
 function GIRS_is_tree(nodeset)
-    # for an indirect graph to be a tree:
-    # 1. no cycles
-    # 2. fully connected
+	# for an indirect graph to be a tree:
+	# 1. no cycles
+	# 2. fully connected
 
-    adj_list = Dict(node.id => node.neighbors for node in values(nodeset))
-    visited = Dict(node.id => false for node in values(nodeset))
-    start_node = first(keys(nodeset))
+	adj_list = Dict(node.id => node.neighbors for node in values(nodeset))
+	visited = Dict(node.id => false for node in values(nodeset))
+	start_node = first(keys(nodeset))
 
-    # depth first search
-    stack = [(start_node,-1)]
-    while !isempty(stack)
-        node, prev = pop!(stack)
-        visited[node] = true
+	# depth first search
+	stack = [(start_node, -1)]
+	while !isempty(stack)
+		node, prev = pop!(stack)
+		visited[node] = true
 
-        for neighbor in adj_list[node]
-            if !visited[neighbor]
-                push!(stack, (neighbor, node))
-            elseif neighbor != prev
-                return false  # Back edge, so it's not a tree
-            end
-        end
-    end
+		for neighbor in adj_list[node]
+			if !visited[neighbor]
+				push!(stack, (neighbor, node))
+			elseif neighbor != prev
+				return false  # Back edge, so it's not a tree
+			end
+		end
+	end
 
-    # Check if all nodes are visited
-    return all(values(visited))
+	# Check if all nodes are visited
+	return all(values(visited))
 end
 
 ###############################################
@@ -157,64 +231,112 @@ end
 ###############################################
 
 function GIRS_has_no_bounce_back_operators(nodeset)
-    for node in values(nodeset)
-        for i in node.neighbors
-            if !isa(node.trans[i,i], ZeroMatrix)
-                return false
-            end
-        end
-    end
-    return true
+	for node in values(nodeset)
+		for i in node.neighbors
+			if !isa(node.trans[i, i], ZeroMatrix)
+				return false
+			end
+		end
+	end
+	return true
 end
 
 #########################
 # Determine tree levels #
 #########################
 
+function determine_tree_hierarchy(nodeset, root)
+	# function assumes nodeset is a tree. Run first GIRS_is_tree
+
+	@assert haskey(nodeset, root)
+	adj_list = Dict(node.id => node.neighbors for node in values(nodeset))
+	not_yet_visited = Dict(node.id => true for node in values(nodeset))
+
+	#breadth first search
+	parent = Dict{UInt, Union{UInt, Nothing}}()
+	parent[root] = nothing
+	levels = [[root]]
+	not_yet_visited[root] = false
+	current_level = levels[1]
+	k = 1
+	while any(values(not_yet_visited))
+		next_level = []
+		for node in current_level
+			for neighbor in adj_list[node]
+				if not_yet_visited[neighbor]
+					parent[neighbor] = node
+					push!(next_level, neighbor)
+					not_yet_visited[neighbor] = false
+				end
+			end
+		end
+		push!(levels, next_level)
+		k += 1
+		current_level = next_level
+	end
+	tree_depth = k - 1
 
 
+	# determine children now...
+	children = Dict{UInt, Set{UInt}}(k => Set([]) for k in keys(nodeset))
+	for k in keys(parent)
+		if !isnothing(parent[k])
+			push!(children[parent[k]], k)
+		end
+	end
 
-# ################
-# # TQS matrices #
-# ################
-# struct TQS{Scalar<:Number}
-#     nodes::Vector{Spinner{Scalar}}
-#     m::Vector{UInt}
-#     n::Vector{UInt}
-#     no_nodes::UInt
-#     M::UInt
-#     N::UInt
-#     index_map::Dict{UInt,UInt}
+	return tree_depth, levels, parent, children
+end
 
-#     # useful tree attributes
-#     # tree_order::Int
-#     # leaf_order::Any
-#     # k_leafs::Vector{Int}
-#     # root_node::Int
-#     # parent::Vector{Union{Int,Nothing}}
-#     # children::Vector{Vector{Int}}
 
-#     function TQS{Scalar}(nodes) where {Scalar<:Number}
-#         # checks
-#         @assert GIRS_is_consistent(nodes)
-#         @assert GIRS_is_tree(nodes)
-#         @assert GIRS_has_no_bounce_back_operators(nodes)
+################
+# TQS matrices #
+################
+# struct TQSMatrix{Scalar <: Number}
+# 	# spinners
+# 	spinners::IndexedVector{Spinner{Scalar}}
+# 	# tree attributes
+# 	node_ordering::Vector{UInt}
+# 	K::UInt
+# 	adjecency_list::IndexedVector{Vector{UInt}}
+# 	root::UInt
+# 	levels::Vector{Vector{UInt}}
+# 	tree_depth::Uint
+# 	# dimensions
+# 	m::IndexedVector{UInt}
+# 	n::IndexedVector{UInt}
+# 	M::UInt
+# 	N::UInt
 
-#         # construct
-#         m = [node.m for node in nodes]
-#         n = [node.n for node in nodes]
-#         no_nodes = length(nodes)
-#         M = sum(m)
-#         N = sum(n)
-#         index_map = Dict(node.id => i for (i, node) in enumerate(nodes))
-#         new{Scalar}(nodes, m, n, no_nodes, M, N, index_map)
-#     end
+# 	function TQSMatrix{Scalar}(spinners, node_ordering, root) where {Scalar <: Number}
+
+# 		# correctness of input dimensions
+#       @assert all([spinners[k].id == k for k in keys(spinners)])            
+# 		@assert length(spinners) == length(node_ordering)
+# 		@assert Set([s.id for s in spinners]) == Set(node_ordering)
+# 		@assert root in node_ordering
+
+# 		# checks spinners generate a valid TQS matrix
+# 		@assert GIRS_is_consistent(spinners)
+# 		@assert GIRS_is_tree(spinners)
+# 		@assert GIRS_has_no_bounce_back_operators(spinners)
+
+# 		# construct
+# 		K = length(nodes_ordering)
+# 		levels = determine_tree_hierarchy(spinners, root)
+# 		m = [node.m for node in nodeset]
+# 		n = [node.n for node in nodeset]
+
+# 		M = sum(m)
+# 		N = sum(n)
+# 		index_map = Dict(node.id => i for (i, node) in enumerate(nodeset))
+# 		new{Scalar}(nodeset, m, n, no_nodes, M, N, index_map)
+# 	end
 # end
-# get_node(T::TQS, i) = T.node[T.index_map[i]]
-# get_m(T::TQS, i) = T.m[T.index_map[i]]
-# get_n(T:::TreeSSS, i) = T.n[T.index_map[i]]
-# Base.:size(T::TQS) = (T.M, T.N)
-
+# Base.:size(T::TQSMatrix) = (T.M, T.N)
+# function Base.:Matrix(T::TQSMatrix)
+# 	return 0
+# end
 
 ##############################
 # TQS matrix vector multiply #
--- tst_TQSMatrices.jl
+++ tst_TQSMatrices.jl
@@ -4,10 +4,54 @@ using LinearAlgebra
 using Test
 
 
-######################################
-# Test 1: construct spinner matrices #
-######################################
+#################################################
+# Test: constructing graph partitioned matrices #
+#################################################
 
+# Considered graph:
+#         3 -- 1 -- 2 
+#              |
+#         4 -- 5 
+
+nodes = [3, 1, 2, 4, 5]
+m = [6, 7, 5, 4, 8]
+n = [5, 8, 4, 3, 7]
+M = sum(m)
+N = sum(n)
+adjacency_list = Dict{UInt, Vector{UInt}}()
+adjacency_list[1] = [2, 3]
+adjacency_list[2] = [1]
+adjacency_list[3] = [1]
+adjacency_list[4] = [5]
+adjacency_list[5] = [1, 4]
+mat = randn(M, N)
+tmp_m = [0; cumsum(m)]
+tmp_n = [0; cumsum(n)]
+
+A = GraphPartitionedMatrix(mat, nodes, m, n, adjacency_list)
+@assert all([A.m[nodes[k]] == m[k] for k in eachindex(nodes)])
+@assert all([A.n[nodes[k]] == n[k] for k in eachindex(nodes)])
+@assert all([A.mrange[nodes[k]] == tmp_m[k]+1:tmp_m[k+1] for k in eachindex(nodes)])
+@assert all([A.nrange[nodes[k]] == tmp_n[k]+1:tmp_n[k+1] for k in eachindex(nodes)])
+@assert all([get_block(A, nodes[k], nodes[l]) == A.mat[tmp_m[k]+1:tmp_m[k+1], tmp_n[l]+1:tmp_n[l+1]] for k in eachindex(nodes), l in eachindex(nodes)])
+nodeset_in = rand(nodes)
+nodeset_out = rand(nodes, 2)
+@assert get_block(A, nodeset_out, nodeset_in) == A.mat[vcat([A.mrange[k] for k in nodeset_out]...), vcat([A.nrange[k] for k in nodeset_in]...)]
+nodeset_in = rand(nodes, 2)
+nodeset_out = rand(nodes)
+@assert get_block(A, nodeset_out, nodeset_in) == A.mat[vcat([A.mrange[k] for k in nodeset_out]...), vcat([A.nrange[k] for k in nodeset_in]...)]
+nodeset_in = rand(nodes, 2)
+nodeset_out = rand(nodes, 2)
+@assert get_block(A, nodeset_out, nodeset_in) == A.mat[vcat([A.mrange[k] for k in nodeset_out]...), vcat([A.nrange[k] for k in nodeset_in]...)]
+nodeset_in = [2 3]
+nodeset_out = [1, 4, 5]
+@assert get_hankelblock(A, nodeset_in) == A.mat[vcat([A.mrange[k] for k in nodeset_out]...), vcat([A.nrange[k] for k in nodeset_in]...)]
+
+####################################
+# Test: construct spinner matrices #
+####################################
+
+# Considered graph:
 #         3 -- 1 -- 2 
 #              |
 #         4 -- 6 -- 5
@@ -29,7 +73,7 @@ m6 = 7
 # state dimensions
 p12 = 3
 p21 = 2
-p13 = 3 
+p13 = 3
 p31 = 2
 p12 = 2
 p21 = 2
@@ -46,14 +90,14 @@ trans = Matrix(undef, 3, 3);
 trans[1, 1], trans[1, 2], trans[1, 3] = ZeroMatrix(p12, p21), rand(p12, p31), rand(p12, p61);
 trans[2, 1], trans[2, 2], trans[2, 3] = rand(p13, p21), ZeroMatrix(p13, p31), rand(p13, p61);
 trans[3, 1], trans[3, 2], trans[3, 3] = rand(p16, p21), rand(p16, p31), ZeroMatrix(p16, p61);
-inp = [rand(p12,m1),
-       rand(p13,m1),
-       rand(p16,m1)];
-out = [rand(n1,p21), rand(n1,p31), rand(n1,p61) ];
+inp = [rand(p12, m1),
+	rand(p13, m1),
+	rand(p16, m1)];
+out = [rand(n1, p21), rand(n1, p31), rand(n1, p61)];
 D = rand(n1, m1);
 node1 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 
-@test node1.trans[2,6] == trans[1,3]
+@test node1.trans[2, 6] == trans[1, 3]
 @test node1.inp[2] == inp[1]
 @test node1.out[3] == out[2]
 @test node1.D == D
@@ -62,13 +106,13 @@ node1 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 id = 2;
 neighbors = [1];
 trans = Matrix(undef, 1, 1);
-trans[1,1] = ZeroMatrix(p21,p12)
+trans[1, 1] = ZeroMatrix(p21, p12)
 inp = [rand(p21, n2)];
-out = [rand(m2,p12)];
+out = [rand(m2, p12)];
 D = rand(m2, n2);
 node2 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 
-@test node2.trans[1,1] == trans[1,1]
+@test node2.trans[1, 1] == trans[1, 1]
 @test node2.inp[1] == inp[1]
 @test node2.out[1] == out[1]
 @test node2.D == D
@@ -76,13 +120,13 @@ node2 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 id = 3;
 neighbors = [1];
 trans = Matrix(undef, 1, 1);
-trans[1,1] = ZeroMatrix(p31,p13)
+trans[1, 1] = ZeroMatrix(p31, p13)
 inp = [rand(p31, n3)];
-out = [rand(m3,p13)];
+out = [rand(m3, p13)];
 D = rand(m3, n3);
 node3 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 
-@test node3.trans[1,1] == trans[1,1]
+@test node3.trans[1, 1] == trans[1, 1]
 @test node3.inp[1] == inp[1]
 @test node3.out[1] == out[1]
 @test node3.D == D
@@ -90,13 +134,13 @@ node3 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 id = 4;
 neighbors = [6];
 trans = Matrix(undef, 1, 1);
-trans[1,1] = ZeroMatrix(p46,p64)
+trans[1, 1] = ZeroMatrix(p46, p64)
 inp = [rand(p46, n4)];
 out = [rand(m4, p64)];
 D = rand(m4, n4);
 node4 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 
-@test node4.trans[6,6] == trans[1,1]
+@test node4.trans[6, 6] == trans[1, 1]
 @test node4.inp[6] == inp[1]
 @test node4.out[6] == out[1]
 @test node4.D == D
@@ -104,37 +148,37 @@ node4 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 id = 5;
 neighbors = [6];
 trans = Matrix(undef, 1, 1);
-trans[1,1] = ZeroMatrix(p56,p65)
+trans[1, 1] = ZeroMatrix(p56, p65)
 inp = [rand(p56, n5)];
 out = [rand(m5, p65)];
 D = rand(m5, n5);
 node5 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 
-@test node5.trans[6,6] == trans[1,1]
+@test node5.trans[6, 6] == trans[1, 1]
 @test node5.inp[6] == inp[1]
 @test node5.out[6] == out[1]
 @test node5.D == D
 
 id = 6;
-neighbors = [1,4,5];
+neighbors = [1, 4, 5];
 trans = Matrix(undef, 3, 3);
 trans[1, 1], trans[1, 2], trans[1, 3] = ZeroMatrix(p61, p16), rand(p61, p46), rand(p61, p56);
 trans[2, 1], trans[2, 2], trans[2, 3] = rand(p64, p16), ZeroMatrix(p64, p46), rand(p64, p56);
 trans[3, 1], trans[3, 2], trans[3, 3] = rand(p65, p16), rand(p65, p46), ZeroMatrix(p65, p56);
-inp = [rand(p61,m6),
-       rand(p64,m6),
-       rand(p65,m6)];
-out = [rand(n6,p16), rand(n6,p46), rand(n6,p56)];
+inp = [rand(p61, m6),
+	rand(p64, m6),
+	rand(p65, m6)];
+out = [rand(n6, p16), rand(n6, p46), rand(n6, p56)];
 D = rand(m6, n6);
 node6 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 
-@test node6.trans[5,4] == trans[3,2]
+@test node6.trans[5, 4] == trans[3, 2]
 @test node6.inp[5] == inp[3]
 @test node6.out[4] == out[2]
 @test node6.D == D
 
 
-nodeset_acyclic = IndexedVector{Spinner}([node1, node2, node3, node4, node5, node6]) 
+nodeset_acyclic = IndexedVector{Spinner}([node1, node2, node3, node4, node5, node6])
 
 # A non tree graph:
 #              1 -- 2 -- 5
@@ -156,9 +200,9 @@ m5 = 5
 # state dimensions
 p12 = 3
 p21 = 2
-p14 = 3 
+p14 = 3
 p41 = 2
-p25 = 3 
+p25 = 3
 p52 = 2
 p14 = 2
 p41 = 2
@@ -173,13 +217,13 @@ neighbors = [2, 4];
 trans = Matrix(undef, 2, 2);
 trans[1, 1], trans[1, 2] = ZeroMatrix(p12, p21), rand(p12, p41)
 trans[2, 1], trans[2, 2] = rand(p14, p21), ZeroMatrix(p14, p41)
-inp = [rand(p12,m1),
-       rand(p14,m1)];
-out = [rand(n1,p21), rand(n1,p31)];
+inp = [rand(p12, m1),
+	rand(p14, m1)];
+out = [rand(n1, p21), rand(n1, p31)];
 D = rand(n1, m1);
 node1 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 
-@test node1.trans[2,4] == trans[1,2]
+@test node1.trans[2, 4] == trans[1, 2]
 @test node1.inp[2] == inp[1]
 @test node1.out[4] == out[2]
 @test node1.D == D
@@ -190,14 +234,14 @@ trans = Matrix(undef, 3, 3);
 trans[1, 1], trans[1, 2], trans[1, 3] = ZeroMatrix(p21, p12), rand(p21, p52), rand(p21, p32);
 trans[2, 1], trans[2, 2], trans[2, 3] = rand(p25, p12), ZeroMatrix(p25, p52), rand(p25, p32);
 trans[3, 1], trans[3, 2], trans[3, 3] = rand(p23, p12), rand(p23, p52), ZeroMatrix(p23, p32);
-inp = [rand(p21,m2),
-       rand(p25,m2),
-       rand(p23,m2)];
-out = [rand(n2,p12), rand(n2,p52), rand(n2,p32)];
+inp = [rand(p21, m2),
+	rand(p25, m2),
+	rand(p23, m2)];
+out = [rand(n2, p12), rand(n2, p52), rand(n2, p32)];
 D = rand(n2, m2);
 node2 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 
-@test node2.trans[3,1] == trans[3,1]
+@test node2.trans[3, 1] == trans[3, 1]
 @test node2.inp[5] == inp[2]
 @test node2.out[1] == out[1]
 @test node2.D == D
@@ -208,13 +252,13 @@ neighbors = [2, 4];
 trans = Matrix(undef, 2, 2);
 trans[1, 1], trans[1, 2] = ZeroMatrix(p32, p23), rand(p32, p43)
 trans[2, 1], trans[2, 2] = rand(p34, p23), ZeroMatrix(p34, p43)
-inp = [rand(p32,m3),
-       rand(p34,m3)];
-out = [rand(n3,p23), rand(n3,p43)];
+inp = [rand(p32, m3),
+	rand(p34, m3)];
+out = [rand(n3, p23), rand(n3, p43)];
 D = rand(n3, m3);
 node3 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 
-@test node3.trans[2,4] == trans[1,2]
+@test node3.trans[2, 4] == trans[1, 2]
 @test node3.inp[4] == inp[2]
 @test node3.out[2] == out[1]
 @test node3.D == D
@@ -225,13 +269,13 @@ neighbors = [1, 3];
 trans = Matrix(undef, 2, 2);
 trans[1, 1], trans[1, 2] = rand(p41, p14), rand(p41, p34)
 trans[2, 1], trans[2, 2] = rand(p43, p14), ZeroMatrix(p43, p34)
-inp = [rand(p41,m4),
-       rand(p43,m4)];
-out = [rand(n4,p14), rand(n4,p34)];
+inp = [rand(p41, m4),
+	rand(p43, m4)];
+out = [rand(n4, p14), rand(n4, p34)];
 D = rand(n4, m4);
 node4 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 
-@test node4.trans[1,3] == trans[1,2]
+@test node4.trans[1, 3] == trans[1, 2]
 @test node4.inp[3] == inp[2]
 @test node4.out[1] == out[1]
 @test node4.D == D
@@ -240,33 +284,66 @@ node4 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 id = 5;
 neighbors = [2];
 trans = Matrix(undef, 1, 1);
-trans[1,1] = ZeroMatrix(p52,p25)
+trans[1, 1] = ZeroMatrix(p52, p25)
 inp = [rand(p52, n5)];
-out = [rand(m5,p25)];
+out = [rand(m5, p25)];
 D = rand(m5, n5);
 node5 = Spinner{Float64}(id, neighbors, trans, inp, out, D);
 
-@test node5.trans[2,2] == trans[1,1]
+@test node5.trans[2, 2] == trans[1, 1]
 @test node5.inp[2] == inp[1]
 @test node5.out[2] == out[1]
 @test node5.D == D
 
-nodeset_cyclic = IndexedVector{Spinner}([node1, node2, node3, node4, node5]) 
+nodeset_cyclic = IndexedVector{Spinner}([node1, node2, node3, node4, node5])
 
-########################################################
-# Test 2: Checking if vector of spinners is consistent #
-########################################################
+######################################################
+# Test: Checking if vector of spinners is consistent #
+######################################################
 
 @assert GIRS_is_consistent(nodeset_acyclic)
 @assert GIRS_is_consistent(nodeset_cyclic)
 
-########################################################
-# Test 3: Checking if vector of spinners is consistent #
-########################################################
+#####################################################################
+# Test: Checking if vector of spinners has no bounce back operators #
+#####################################################################
 
 @assert GIRS_has_no_bounce_back_operators(nodeset_acyclic)
 @assert !GIRS_has_no_bounce_back_operators(nodeset_cyclic)
 
-#######################################################################
-# Test 4: Checking if vector of spinners has no bounce back operators #
-#######################################################################
+##################################
+# Test: determine tree hierarchy #
+##################################
+
+# Considered graph:
+#         3 -- 1 -- 2 
+#              |
+#         4 -- 6 -- 5
+
+tree_depth, levels, parent, children = determine_tree_hierarchy(nodeset_acyclic, 1)
+@assert tree_depth == 2
+@assert Set(levels[1]) == Set([1])
+@assert Set(levels[2]) == Set([3, 2, 6])
+@assert Set(levels[3]) == Set([4, 5])
+@assert isnothing(parent[1])
+@assert parent[2] == 1
+@assert parent[6] == 1
+@assert children[1] == Set([3, 6, 2])
+@assert children[6] == Set([4, 5])
+@assert children[2] == Set([])
+
+tree_depth, levels, parent, children = determine_tree_hierarchy(nodeset_acyclic, 2)
+@assert tree_depth == 3
+@assert Set(levels[1]) == Set([2])
+@assert Set(levels[2]) == Set([1])
+@assert Set(levels[3]) == Set([3, 6])
+@assert Set(levels[4]) == Set([4, 5])
+@assert children[2] == Set([1])
+@assert children[1] == Set([3, 6])
+@assert children[6] == Set([4, 5])
+@assert children[5] == Set([])
+
+
+###############################
+# Test: determine tree levels #
+###############################
